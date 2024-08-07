ac8f234a61afae9b75b70b2e8a206e903d3b8cb9
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-08-07 22:17:21 +0600

    feat: implement login logic

--- backend/configs.py
+++ backend/configs.py
@@ -9,3 +9,8 @@ DB_NAME = os.getenv("DB_NAME", "localhost")
 DB_PASS = os.getenv("DB_PASS", "localhost")
 DB_USER = os.getenv("DB_USER", "localhost")
 DB_DRIVER = os.getenv("DB_DRIVER", "localhost")
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", None)
+
+
+if not JWT_SECRET_KEY:
+    raise Exception("JWT_SECRET_KEY env is required")
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,8 +1,17 @@
+import jwt
+from datetime import timedelta, timezone, datetime
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from database.db import InjectDB
+from database import InjectDB, User
+from sqlalchemy.sql import select
+from sqlalchemy.exc import NoResultFound
+from passlib.context import CryptContext
+from configs import JWT_SECRET_KEY
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 class LoginPayload(BaseModel):
@@ -17,9 +26,41 @@ class RegisterPayload(BaseModel):
     fullname: str
 
 
+def verify_pwd(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+
+def get_pwd_hash(plain: str) -> str:
+    return pwd_context.hash(plain)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    exp = datetime.now(timezone.utc) + expires_delta if expires_delta else timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": exp})
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY)
+    return encoded_jwt
+
+
 @auth_router.post("/login")
 async def login(payload: LoginPayload, db: InjectDB):
-    return None
+    try:
+        async with db:
+            q = select(User).where(User.email == payload.email)
+            result = await db.session.execute(q)
+            user = result.scalar_one_or_none()
+
+        if not user:
+            return JSONResponse({"message": f"No user found with email: {payload.email}"}, 401)
+
+        if not verify_pwd(payload.password, user.password):
+            return JSONResponse({"message": "Invalid password"}, 401)
+
+        return JSONResponse({}, 200)
+    except NoResultFound:
+        return JSONResponse({"message": f"No user found with email {payload.email}"}, 401)
+    except Exception as e:
+        return JSONResponse({"message": str(e)}, 500)
 
 
 @auth_router.post("/register")
