5720a219921e50ffeb8a53ed1df7d98ef1688341
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-08-10 16:42:46 +0600

    better migrations with alembic

--- backend/database/profile_section.py
+++ backend/database/profile_section.py
@@ -1,5 +1,9 @@
-from uuid import uuid4
-from sqlalchemy import UUID, Column, String
+from typing import List
+from datetime import datetime
+from uuid import uuid4, UUID
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy import UUID as SqlUUID, String, ForeignKey, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from . import Base
 
 
@@ -7,9 +11,44 @@ class ProfileSection(Base):
     __tablename__ = "profile_sections"
     __table_args__ = {"schema": "reformify"}
 
-    id = Column(
-        UUID(as_uuid=True), primary_key=True, default=uuid4(), unique=True, index=True
+    id: Mapped[UUID] = mapped_column(
+        SqlUUID(as_uuid=True),
+        primary_key=True,
+        unique=True,
+        default=uuid4,
     )
-    title = Column(String(length=256))
-    # user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=True)
-    # user = relationship("User", back_populates="sections")
+    title: Mapped[str] = mapped_column(String(length=256), nullable=False)
+    user_id: Mapped[UUID] = mapped_column(
+        SqlUUID(as_uuid=True), ForeignKey("reformify.users.id"), nullable=False
+    )
+    user: Mapped[List["User"]] = relationship(back_populates="sections")
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=func.now(), onupdate=func.now()
+    )
+
+    def __init__(self, *, user_id: UUID, title: str):
+        self.title = title
+        self.user_id = user_id
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "title": self.title,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
+
+    def serialize(self):
+        return BaseProfileSection(**self.to_dict())
+
+
+class BaseProfileSection(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    user_id: UUID
+    title: str
+    created_at: datetime
+    updated_at: datetime
--- backend/database/user.py
+++ backend/database/user.py
@@ -1,6 +1,9 @@
+from typing import List
+from uuid import uuid4, UUID
+from datetime import datetime
 from pydantic import BaseModel, ConfigDict, Field
-from uuid import uuid4
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, String, func, UUID as SqlUUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from . import Base
 
 
@@ -8,32 +11,64 @@ class User(Base):
     __tablename__ = "users"
     __table_args__ = {"schema": "reformify"}
 
-    id: Mapped[str] = mapped_column(primary_key=True, unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(index=True, unique=True, nullable=False)
-    fullname: Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[UUID] = mapped_column(
+        SqlUUID(as_uuid=True),
+        primary_key=True,
+        unique=True,
+        default=uuid4,
+    )
+    email: Mapped[str] = mapped_column(
+        String(400), index=True, unique=True, nullable=False
+    )
+    fullname: Mapped[str] = mapped_column(String(100), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     deleted: Mapped[bool] = mapped_column(default=False)
+    sections: Mapped[List["ProfileSection"]] = relationship(back_populates="user")
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=func.now(), onupdate=func.now()
+    )
 
     def __init__(self, *, email: str, fullname: str, password: str):
-        self.id = uuid4().hex
         self.email = email
         self.fullname = fullname
         self.password = password
 
-    def serialize(self) -> dict:
+    def to_dict(self) -> dict:
         return {
             "id": self.id,
             "email": self.email,
             "fullname": self.fullname,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
         }
+
+    def to_dict_full(self) -> dict:
+        return {
+            "id": self.id,
+            "email": self.email,
+            "fullname": self.fullname,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+            "deleted": self.deleted,
+            "password": self.password,
+        }
+
+    def serialize(self):
+        return BaseUser(**self.to_dict())
+
+    def serialize_full(self):
+        return BaseUserPrivate(**self.to_dict_full())
 
 
 class BaseUser(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    id: str
+    id: UUID
     email: str
     fullname: str
+    created_at: datetime
+    updated_at: datetime
 
 
 class BaseUserPrivate(BaseUser):
