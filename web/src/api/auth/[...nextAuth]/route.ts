b7d094f09a59a7725ee50e61a4be53fa5e31c6bf
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-08-18 13:12:30 +0600

    feat: implement nextauth

--- web/src/api/auth/[...nextAuth]/route.ts
+++ web/src/api/auth/[...nextAuth]/route.ts
@@ -27,15 +27,12 @@ export const authOptions: AuthOptions = {
             `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
             {
               method: "POST",
-              body: JSON.stringify({
-                email: credentials?.email,
-                password: credentials?.password,
-              }),
+              body: JSON.stringify(credentials || {}),
               headers: { "Content-Type": "application/json" },
             },
           );
           const data = await res.json();
-          if (data.user && data.user.email) {
+          if (res.ok && data.user && data.user.email) {
             return data.user;
           }
           return null;
@@ -48,32 +45,30 @@ export const authOptions: AuthOptions = {
   ],
 
   callbacks: {
-    jwt: async ({ token, user }) => {
-      if (user) {
-        token.user = user;
-        token.accessToken = user.token;
-      }
-      // Decode the JWT to access its payload
-      if (token.accessToken) {
-        const decoded = jwt.decode(token.accessToken);
-        const now = Math.floor(Date.now() / 1000);
-        if (
-          decoded &&
-          typeof decoded === "object" &&
-          "exp" in decoded &&
-          decoded.exp &&
-          decoded.exp < now
-        ) {
-          // Invalidate the token by deleting the accessToken
-          delete token.accessToken;
-        }
+    jwt: async ({ token, user, account }) => {
+      console.log("from jwt:", token, user, account);
+      if (user && user.access_token) {
+        token.accessToken = user.access_token;
+      } else if (account && account.access_token) {
+        token.accessToken = account.access_token;
       }
       return token;
     },
 
     session: async ({ session, token, user }) => {
-      console.log(session, token, user);
+      console.log("from session:", session, token, user);
+      session.access_token = token.access_token;
       return session;
+    },
+
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log("from signIn:", user, account, profile, email, credentials);
+      return false;
+    },
+
+    async redirect({ url, baseUrl }) {
+      console.log("from redirect:", url, baseUrl);
+      return baseUrl;
     },
   },
 };
--- web/src/components/LoginForm.tsx
+++ web/src/components/LoginForm.tsx
@@ -1,20 +1,50 @@
 "use client";
+import { type SyntheticEvent, useState } from "react";
 import { BsKeyFill, BsEnvelopeFill } from "react-icons/bs";
+import { signIn } from "next-auth/react";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function login(e: SyntheticEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoading(true);
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    if (res?.error) {
+      setLoading(false);
+    } else {
+      console.log("login successful:", res);
+    }
+  }
+
   return (
     <form
       action="submit"
-      onSubmit={(e) => e.preventDefault()}
+      onSubmit={login}
       className="w-full flex flex-col gap-6 p-6 max-w-[90%] md:max-w-xl"
     >
       <label className="dui-input dui-input-bordered flex items-center gap-2">
         <BsEnvelopeFill />
-        <input type="text" className="grow" placeholder="Email" required />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+          type="text"
+          className="grow"
+          placeholder="Email"
+          required
+        />
       </label>
       <label className="dui-input dui-input-bordered flex items-center gap-2">
         <BsKeyFill />
         <input
+          value={email}
+          onChange={(e) => setEmail(e.currentTarget.value)}
           type="password"
           className="grow"
           placeholder="Password"
