838e9c73a767aa0ebc0f2c5cdfc2850671bada02
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-08-23 22:34:20 +0600

    add middleware to protect routes

--- web/src/app/middleware.ts
+++ web/src/app/middleware.ts
@@ -5,18 +5,12 @@ import {
 } from "next/server";
 
 export async function middleware(req: NextRequest) {
+    console.log("middleware running");
     return NextResponse.next();
 }
 
 export const config: MiddlewareConfig = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - api (API routes)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-         */
         "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|auth).*)",
     ],
 };
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,14 +3,17 @@ import { FaGithub } from "react-icons/fa";
 
 export default function HomePage() {
     return (
-        <main className="w-full flex flex-col justify-center items-center min-h-screen">
+        <main className="w-full flex flex-col justify-center items-center min-h-screen p-6">
             <h2 className="text-4xl font-bold mb-8">Reformify</h2>
 
-            <div className="w-full max-w-[90%] lg:max-w-xl flex flex-col items-center gap-6">
-                <Link href="/auth/login" className="dui-btn dui-btn-primary">
+            <div className="w-full max-w-[300px] flex flex-col items-center gap-6">
+                <Link
+                    href="/auth/login"
+                    className="dui-btn dui-btn-primary w-full"
+                >
                     Login with Password
                 </Link>
-                <Link href="/auth/login" className="dui-btn">
+                <Link href="/api/auth/signin" className="dui-btn w-full">
                     <FaGithub />
                     Continue with GitHub
                 </Link>
