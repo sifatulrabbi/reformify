67516a68a14044da196ae31e79cf586d41f2d5e9
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-09-09 16:09:18 +0600

    add new routes

--- backend/configs.py
+++ backend/configs.py
@@ -11,6 +11,7 @@ DB_PASS = os.getenv("DB_PASS", None)
 DB_USER = os.getenv("DB_USER", None)
 DB_DRIVER = os.getenv("DB_DRIVER", None)
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", None)
+INTERNAL_API_KEY = os.getenv("INTERNAL_API_KEY", None)
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
 
@@ -27,3 +28,5 @@ if not DB_USER:
     raise Exception("DB_USER env is required")
 if not DB_DRIVER:
     raise Exception("DB_DRIVER env is required")
+if not INTERNAL_API_KEY:
+    raise Exception("INTERNAL_API_KEY env is required")
